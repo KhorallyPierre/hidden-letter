6d201fff4aec5334787d9ac3f0ce18a3e557f434
Khorally <khorally.pierre@gmail.com>
2023-02-07 16:04:44 -0500

    game setup

---
+++ src/utils/createBoard.js
@@ -0,0 +1,23 @@
+// contains hidden letter and other letters
+import {createCell} from "./createCell";
+const couples = [
+    ["B", "D"],
+    ["B", "3"],
+    ["g", "q"],
+    ["G", "6"],
+    ["L", "I"],
+    ["O", "0"],
+    ["V", "U"],
+    ["Z", "2"],
+    ["F", "E"],
+    ["K", "X"],
+    ["p", "q"],
+    ["B", "8"],
+    ["W", "M"],
+    ["S", "5"]
+
+]
+export function createBoard(){
+ const board = [];
+ const randomwRow = Math.floor(Math.random()* 25)
+}
