e197f31fb53a51b5e11ba537130d68c36603d517
Khorally-s <kvpierre@gmail.com>
2023-05-01 18:29:52 -0400

    you win

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ import { createBoard } from './utils/createBoard';
 function App() {
   const [board, setBoard] = React.useState(() => createBoard());
   const [gamesToWin, setGamesToWin] = React.useState(6);
+  const [timer, setTimer] = React.useState(0);
+  const miliseconds = (timer / 10).toFixed(2);
+
+
+  React.useEffect(() => {
+    if (gamesToWin > 0) {
+      const interval = setInterval(() => {
+        setTimer(timer + 1)
+      }, 100)
+      return () => clearInterval(interval);
+    }
+  }, [gamesToWin, timer])
 
   function handleClick(row, col) {
     if (board[row][col].isHidden) {
@@ -20,15 +32,32 @@ function App() {
   return (
     <div className="App">
       <h1> Find the Hidden Letter</h1>
-      {board.map((row, rowIdx) => (
-        <div key={rowIdx} className={'row'}>
-          {row.map((letter, letterIdx) => (
-            <Cell key={letterIdx} handleClick={handleClick} {...letter}> Cell </Cell>
-          ))}
-        </div>
-      ))}
+      {gamesToWin > 0 && <p> Time: {miliseconds}</p>}
+      {gamesToWin === 0 ? (
+        <p> Congratulations, your time was: {miliseconds}</p>
+      ) : (
+        <p> Games to win: {gamesToWin}</p>
+      )}
+      <div className="lettersContainer">
+        {gamesToWin > 0 && (
+          <div>
+            {board.map((row, rowIdx) => (
+              <div key={rowIdx} className={'row'}>
+                {row.map((letter, letterIdx) => (
+                  <Cell key={letterIdx} handleClick={handleClick} {...letter}> Cell </Cell>
+                ))}
+              </div>
+            ))}
+
+
+          </div>)}
+
+      </div>
+
     </div>
+
   );
+
 }
 
 export default App;
